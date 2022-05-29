8c75060b5e0d3a5dff94a22b436842189efba1c3
jeffersonreis <33864468+jeffersonreis@users.noreply.github.com>
2022-05-29 20:12:29 -0300

    .

--- stech.ino
+++ stech.ino
@@ -2,7 +2,6 @@
 
 #include <WiFi.h>
 #include <HTTPClient.h>
-// #include <ArduinoJson.h>
 
 #define PIN_TRIG 4
 #define PIN_ECHO 2
@@ -11,6 +10,7 @@
 Servo myservo; 
 int pos = 90;   
 int buttonState = 0; 
+int distanceOpen = 0;
 
 const char* ssid = "Wokwi-GUEST";
 const char* password = "";
@@ -18,12 +18,11 @@ const String url = "https://raw.githubusercontent.com/the-ufsc/the-door/main/dat
 
 void setup() {
   Serial.begin(115200);
-  Serial.print("Connect Components");
+  Serial.println("Connect Components");
   pinMode(PIN_TRIG, OUTPUT);
   pinMode(PIN_ECHO, INPUT);
   myservo.attach(PIN_SERVO);
-  Serial.print("Components ok.");  
-
+  Serial.println("Components ok.");  
   
   Serial.print("Connecting to WiFi");
   WiFi.begin(ssid, password, 6);
@@ -32,31 +31,25 @@ void setup() {
     Serial.print(".");
   }
 
-  Serial.print("Wifi ok.");
-
+  Serial.println("Wifi ok.");
   
   HTTPClient http;
   http.begin(url);
 
   int httpResponseCode = http.GET();
   if (httpResponseCode > 0) {
-    Serial.print("HTTP ");
-    Serial.println(httpResponseCode);
-    String payload = http.getString();
-    Serial.println();
-    Serial.println(payload);
+    distanceOpen = http.getString().toInt();
+    Serial.print("Distancia definida:");
+    Serial.println(distanceOpen);
+    Serial.println("Pronto para uso");
   }
   else {
-    Serial.print("Error code: ");
-    Serial.println(httpResponseCode);
-    Serial.println(":-(");
+    Serial.println("ERROR HTTP");
   }
-
 }
 
 
 void loop() {
-
   // CAPTURAR A DISTANCIA //
   digitalWrite(PIN_TRIG, HIGH);
   delayMicroseconds(10);
@@ -66,7 +59,7 @@ void loop() {
   int duration = pulseIn(PIN_ECHO, HIGH);
   
   // END DISTANCIA //
-  if (duration / 58 < 200){
+  if (duration / 58 < distanceOpen){
    pos = 180;
   } else {
     pos = 90; 
