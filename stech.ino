d0895bf64bd6b2e8a1fd178ff03c5200b69dc7fe
jeffersonreis <33864468+jeffersonreis@users.noreply.github.com>
2022-05-29 17:47:16 -0300

    att

--- stech.ino
+++ stech.ino
@@ -1,26 +1,60 @@
-#include <Servo.h>
+#include <ESP32Servo.h>
 
-#define PIN_TRIG 13
-#define PIN_ECHO 11
-#define PIN_BUTTON 2
-#define PIN_SERVO 10
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+#define PIN_TRIG 4
+#define PIN_ECHO 2
+#define PIN_SERVO 15
 
 Servo myservo; 
 int pos = 90;   
 int buttonState = 0; 
 
-const String link = "http://example.org";
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
+const String url = "https://raw.githubusercontent.com/the-ufsc/the-door/main/data.json";
 
 void setup() {
-  pinMode(PIN_BUTTON, INPUT_PULLUP);
-  // Serial.begin(115200);
+  Serial.begin(115200);
+  Serial.print("Connect Components");
   pinMode(PIN_TRIG, OUTPUT);
   pinMode(PIN_ECHO, INPUT);
   myservo.attach(PIN_SERVO);
+  Serial.print("Components ok.");  
+
+  
+  Serial.print("Connecting to WiFi");
+  WiFi.begin(ssid, password, 6);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+
+  Serial.print("Wifi ok.");
+
+  
+  HTTPClient http;
+  http.begin(url);
+
+  int httpResponseCode = http.GET();
+  if (httpResponseCode > 0) {
+    Serial.print("HTTP ");
+    Serial.println(httpResponseCode);
+    String payload = http.getString();
+    Serial.println();
+    Serial.println(payload);
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+    Serial.println(":-(");
+  }
+
 }
 
+
 void loop() {
-  buttonState = digitalRead(PIN_BUTTON);
 
   // CAPTURAR A DISTANCIA //
   digitalWrite(PIN_TRIG, HIGH);
